154fb37776563791e6e3925e54d765d5cfae8663
porLnwza <por.stw50@gmail.com>
2024-06-26 12:26:41 +0700

    Add files via upload

---
+++ Triangle.py
@@ -0,0 +1,7 @@
+def Triangle(base,height):
+    area = 1/2*base*height
+    #print("พื้นที่สามเหลี่ยม %.2f" % area)
+    return area
+    
+print("พื้นที่สามเหลี่ยม %.2f" % Triangle(2,3))
+print("พื้นที่สามเหลี่ยม %.2f" %Triangle(5,10))
---
+++ tast1.py
@@ -0,0 +1,4 @@
+person = ["Sutiwat",16,171,64,"CDTI"]
+print(person)
+print(person[2])
+#print(person[5])
