c6c5c4d4744249965e3c9502d0b42f8931ef3b6c
porLnwza <por.stw50@gmail.com>
2024-09-04 10:32:24 +0700

    Delete Triangle.py

--- Triangle.py
+++
@@ -1,7 +0,0 @@
-def Triangle(base,height):
-    area = 1/2*base*height
-    #print("พื้นที่สามเหลี่ยม %.2f" % area)
-    return area
-    
-print("พื้นที่สามเหลี่ยม %.2f" % Triangle(2,3))
-print("พื้นที่สามเหลี่ยม %.2f" %Triangle(5,10))
